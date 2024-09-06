683a04650dc8d404cd8b14d9bf0af637ae83a06f
Ivan chiari <Ivan31@mhs.unsyiah.ac.id>
2024-09-06 14:39:19 +0700

    selesai tugas 1

--- app.py
+++ app.py
@@ -1,97 +1,5 @@
-# from flask import Flask, request, render_template, redirect, url_for
-# import cv2
-# import numpy as np
-# import os
-# from matplotlib import pyplot as plt
-# from werkzeug.utils import secure_filename
-
-# app = Flask(__name__)
-
-# # Set up the upload folder
-# UPLOAD_FOLDER = 'static/uploads'
-# os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-# # Function to save and return image path
-# def save_image(image, filename):
-#     path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-#     cv2.imwrite(path, image)
-#     return path
-
-# # Function to create color histogram
-# def create_color_histogram(image, hist_path):
-#     plt.figure()
-#     colors = ('b', 'g', 'r')
-#     for i, color in enumerate(colors):
-#         hist = cv2.calcHist([image], [i], None, [256], [0, 256])
-#         plt.plot(hist, color=color)
-#         plt.xlim([0, 256])
-#     plt.title('Color Histogram')
-#     plt.xlabel('Intensity Value')
-#     plt.ylabel('Frequency')
-#     plt.grid(True)
-#     plt.savefig(hist_path)
-#     plt.close()
-
-# # Function to perform histogram equalization on a color image
-# def histogram_equalization(image_path):
-#     image = cv2.imread(image_path)
-#     img_yuv = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
-#     img_yuv[:, :, 0] = cv2.equalizeHist(img_yuv[:, :, 0])
-#     equalized_image = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
-#     return equalized_image
-
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     if request.method == 'POST':
-#         # Check if the post request has the file part
-#         if 'file' not in request.files:
-#             return redirect(request.url)
-#         file = request.files['file']
-#         # If the user does not select a file, the browser submits an empty file without a filename
-#         if file.filename == '':
-#             return redirect(request.url)
-#         if file:
-#             # Save the file
-#             filename = secure_filename(file.filename)
-#             image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-#             file.save(image_path)
-
-#             # Read the original image and create color histogram
-#             original_image = cv2.imread(image_path)
-#             original_hist_path = os.path.join(app.config['UPLOAD_FOLDER'], 'original_hist.png')
-#             create_color_histogram(original_image, original_hist_path)
-
-#             # Paths for equalized image and histogram
-#             equalized_image_path = os.path.join(app.config['UPLOAD_FOLDER'], 'equalized.jpg')
-#             equalized_hist_path = os.path.join(app.config['UPLOAD_FOLDER'], 'equalized_hist.png')
-
-#             # Check if equalization button was clicked
-#             if 'equalize' in request.form:
-#                 equalized_image = histogram_equalization(image_path)
-#                 save_image(equalized_image, 'equalized.jpg')
-
-#                 # Create color histogram for equalized image
-#                 create_color_histogram(equalized_image, equalized_hist_path)
-
-#                 return render_template('index.html',
-#                                        original_image=url_for('static', filename=f'uploads/{filename}'),
-#                                        equalized_image=url_for('static', filename='uploads/equalized.jpg'),
-#                                        original_hist=url_for('static', filename='uploads/original_hist.png'),
-#                                        equalized_hist=url_for('static', filename='uploads/equalized_hist.png'))
-
-#             return render_template('index.html', 
-#                                    original_image=url_for('static', filename=f'uploads/{filename}'),
-#                                    original_hist=url_for('static', filename='uploads/original_hist.png'))
-
-#     return render_template('index.html')
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
 from flask import Flask, render_template, request, redirect, url_for
 from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
-import numpy as np
 import cv2
 import os
 from io import BytesIO
@@ -103,16 +11,20 @@ UPLOAD_FOLDER = 'static'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Fungsi untuk membuat plot histogram
+# Fungsi untuk membuat plot histogram
 def plot_histogram(image, title):
     fig = Figure(figsize=(5, 3))
     ax = fig.add_subplot(111)
     
-
-    # Hitung histogram untuk setiap channel (B, G, R)
-    for i, color in enumerate(['b', 'g', 'r']):
-        hist = cv2.calcHist([image], [i], None, [256], [0, 256])
-        ax.plot(hist, color=color)
-        
+    if len(image.shape) == 2:  # Grayscale image
+        hist = cv2.calcHist([image], [0], None, [256], [0, 256])
+        ax.plot(hist, color='gray')
+    else:  # RGB image
+        # Hitung histogram untuk setiap channel (B, G, R)
+        for i, color in enumerate(['b', 'g', 'r']):
+            hist = cv2.calcHist([image], [i], None, [256], [0, 256])
+            ax.plot(hist, color=color)
+    
     ax.set_title(title)
     ax.set_xlabel("Intensity Value")
     ax.set_ylabel("Frequency")
@@ -125,6 +37,7 @@ def plot_histogram(image, title):
     data = base64.b64encode(buf.getbuffer()).decode("ascii")
         
     return f"data:image/png;base64,{data}"
+
     
 # Fungsi untuk histogram equalization
 def histogram_equalization(image):
@@ -136,9 +49,10 @@ def histogram_equalization(image):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        if 'file' not in request.files:
+        if 'file' not in request.files or 'color_mode' not in request.form:
             return redirect(request.url)
         file = request.files['file']
+        color_mode = request.form['color_mode']  # Grayscale or RGB
         if file.filename == '':
             return redirect(request.url)
         if file:
@@ -147,13 +61,16 @@ def index():
             file.save(filepath)
 
             # Baca gambar
-            image = cv2.imread(filepath)
+            if color_mode == 'grayscale':
+                image = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+            else:
+                image = cv2.imread(filepath)
 
             # Plot histogram asli
             original_hist = plot_histogram(image, 'Original Histogram')
 
             # Histogram equalization
-            equalized_image = histogram_equalization(image)
+            equalized_image = histogram_equalization(image) if color_mode == 'rgb' else cv2.equalizeHist(image)
             equalized_hist = plot_histogram(equalized_image, 'Equalized Histogram')
 
             # Konversi gambar hasil equalization ke base64 untuk ditampilkan di HTML
@@ -164,7 +81,8 @@ def index():
                                    original_image=file.filename, 
                                    original_hist=original_hist,
                                    equalized_image=f"data:image/png;base64,{equalized_image_data}",
-                                   equalized_hist=equalized_hist)
+                                   equalized_hist=equalized_hist,
+                                   color_mode=color_mode)
     return render_template('index.html')
 
 if __name__ == "__main__":
